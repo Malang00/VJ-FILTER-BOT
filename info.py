9b76e8b30fb42cf61cdbdd37dfc3bf02600e30c9
Jaat <91902331+jaat9512@users.noreply.github.com>
2024-12-21 22:44:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -117,7 +117,7 @@ QUALITIES = ["360p", "480p", "720p", "1080p", "1440p", "2160p"]
 
 
 # Online Stream and Download
-STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or Flase
+STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
@@ -127,7 +127,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://mytestvj-998c9929dc7a.herokuapp.com/")
+URL = environ.get("URL", "jaatmoviebot-57da50738dce.herokuapp.com/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
